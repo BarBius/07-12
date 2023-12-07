869a8291fed63c81326d6af77b616d5726fda2e5
BarBius <145337329+BarBius@users.noreply.github.com>
2023-12-07 14:44:10 +0100

    Update Etudiant.js

--- Etudiant.js
+++ Etudiant.js
@@ -1,9 +1,1 @@
-const mongoose = require('mongoose');
 
-const etudiantSchema = new mongoose.Schema({
-nom: String,
-prenom: String,
-classe: String
-});
-
-module.exports = mongoose.model('Etudiant', etudiantSchema);
